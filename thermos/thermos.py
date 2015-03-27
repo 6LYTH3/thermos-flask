5cce13e2c0ce9a948e457e36e5794ea36f20f636
xarisd <xaris.dimitriou@gmail.com>
2015-03-27 12:07:52 +0200

    Add functionality for saving a bookmark
    * Use POST - REDIRECT - GET
    * Use request(), redirect(), url_for() from flask

--- thermos/thermos.py
+++ thermos/thermos.py
@@ -1,6 +1,47 @@
-from flask import Flask, render_template
+from datetime import datetime
+from flask import Flask, render_template, request, redirect, url_for
+from logging import DEBUG
 
 app = Flask(__name__)
+app.logger.setLevel(DEBUG)
+
+bookmarks = []
+
+
+def store_bookmark(url):
+    bookmarks.append(dict(
+        url=url,
+        user="xarisd",
+        date=datetime.utcnow()
+    ))
+
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                           title='Title passed from view to template',
+                           user=User('Haris', 'Dimitriou'))
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_bookmark():
+    if request.method == "POST":
+        url = request.form['url']
+        store_bookmark(url)
+        app.logger.debug('store url: ' + url)
+        return redirect(url_for('index'))
+    return render_template('add.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+
+@app.errorhandler(500)
+def server_error(e):
+    return render_template('500.html'), 500
 
 
 class User:
@@ -13,29 +54,6 @@ class User:
 
     def __str__(self):
         return "{} {}".format(self.firstname, self.lastname)
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return render_template('index.html',
-                           title='Title passed from view to template',
-                           user=User('Haris', 'Dimitriou'))
-
-
-@app.route('/add')
-def add_bookmark():
-    return render_template('add.html')
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    return render_template('404.html'), 404
-
-
-@app.errorhandler(500)
-def server_error(e):
-    return render_template('500.html'), 500
 
 
 if __name__ == '__main__':
