29c8e9f5d8dd481d5a47f733fd5ed2888be3ebfb
xarisd <xaris.dimitriou@gmail.com>
2015-03-30 20:42:57 +0300

    Add an app factory method that can load dynamic configuration

--- manage.py
+++ manage.py
@@ -1,13 +1,14 @@
 #! /usr/bin/env python
+import os
 
-from thermos import app, db
+from thermos import create_app, db
 from thermos.models import User, Bookmark, Tag
 from flask.ext.script import Manager, prompt_bool
 from flask.ext.migrate import Migrate, MigrateCommand
 
+app = create_app(os.getenv('THERMOS_ENV') or 'dev')
 manager = Manager(app)
 migrate = Migrate(app, db)
-
 manager.add_command('db', MigrateCommand)
 
 
---
+++ thermos/config.py
@@ -0,0 +1,32 @@
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
+class Config:
+    SECRET_KEY = 'lalakis'
+    DEBUG = False
+
+
+class DevelopmentConfig(Config):
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+
+
+class TestingConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' +\
+        os.path.join(basedir, 'data-test.sqlite')
+    WTF_CSRF_ENABLED = False
+
+
+class ProductionConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'thermos.db')
+
+
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
+)
